44deb8aa7898d46c898144b2a8b76f16a2e2a489
Joshua Philips <jmichaelphilips@gmail.com>
2016-06-23 12:52:30 -0500

    Added some functionality to the buttons

--- src/Maths.js
+++ src/Maths.js
@@ -21,3 +21,34 @@ function square(x) {
 function sqRoot(x) {
 	return Math.sqrt(x);
 };
+
+var buttons = document.getElementsByTagName("button");
+var userInput = document.getElementById("userInput");
+var equals = document.getElementById("equals")
+
+console.log(buttons)
+
+var currentButton = null;
+var xNum = null;
+var yNum = null;
+
+window.addEventListener("click", function(){
+	if (event.target.tagName == "BUTTON") {
+	currentButton = event.target.value;
+	xNum = userInput.value;
+	console.log(xNum)
+	userInput.value = "";
+	};
+});
+
+equals.addEventListener("click", function(){
+	yNum = userInput.value;
+
+	return currentButton(xNum, yNum);
+});
+
+
+
+
+
+
